368890e54d231e9cac4d4a39e1ad0069c8a8baff
James Summers <jamesnsummers@gmail.com>
2017-05-25 15:04:22 -0500

    added 3 new tests

--- node-mocha-testing-intro/starter-code/app/test/candies_tests.js
+++ node-mocha-testing-intro/starter-code/app/test/candies_tests.js
@@ -13,4 +13,45 @@ describe("Candies", function(){
     .set("Accept", "application/json")
     .expect(200, done)
   });
+
+  it("should return an array", function(done){
+    api
+      .get("/candies")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body).to.be.an('array');
+        done()
+      });
+    });
+
+    it("should return an object that have a field called 'name' ", function(done){
+    api
+      .get("/candies")
+      .set("Accept", "application/json")
+      .end(function(error, response){
+        expect(response.body[0]).to.have.property('name');
+        done()
+    });
+  });
+});
+
+describe("POST /candies", function(){
+  before(function(done){
+   api
+   .post("/candies")
+   .set("Accept", "application/json")
+   .send({
+     "id": 5,
+     "name": "Lollipop",
+     "color": "Red"
+   }).end(done)
+ });
+ it("should add a candy object to the collection candies and return it", function(done){
+  api.get("/candies")
+  .set("Accept", "application/json")
+  .end(function(error, response){
+    expect(response.body.length).to.equal(5);
+    done()
+    });
+  });
 });
